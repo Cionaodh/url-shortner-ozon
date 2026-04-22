c35202d80c1cb33fef41fa32098a11d14d59f147
Dmitry Gorlanov <dmitrygorlanov0@gmail.com>
2026-04-22 13:58:39 +0300

    добавил обертку ошибок

---
+++ internal/controller/http/error.go
@@ -0,0 +1,16 @@
+package http
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type ErrorResp struct {
+	Error string `json:"error"`
+}
+
+func errorResponse(w http.ResponseWriter, msg string, status int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(ErrorResp{Error: msg})
+}
